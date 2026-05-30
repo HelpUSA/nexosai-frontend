15866e2b2bfe8015f4489a7c4df75cdde8e64af4
Wagner Medeiros dos Santos <helpus.ecommerce@gmail.com>
2026-05-30 14:57:50 -0300

    allow multiple admin emails for login

--- src/auth.ts
+++ src/auth.ts
@@ -4,16 +4,16 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
- providers: [
- Google({
- clientId: process.env.GOOGLE_CLIENT_ID || "",
- clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
- }),
- ],
- callbacks: {
- async signIn({ user }) {
- return user.email === "helpus.ecommerce@gmail.com";
- },
- },
- secret: process.env.AUTH_SECRET,
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      return ["helpus.ecommerce@gmail.com", "admin@helpusbr.com"].includes(user.email || "");
+    },
+  },
+  secret: process.env.AUTH_SECRET,
 });
